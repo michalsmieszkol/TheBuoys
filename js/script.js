f3eb8527d557db20bc9b2a902d51522da1ad5a99
Michal <michalsmieszkol@gmail.com>
2022-12-14 09:17:38 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 	
     const nav = document.querySelector('.navbar');
+	const allNavItems = document.querySelectorAll(`.nav-link`)
+	const navList = document.querySelector(`.navbar-collapse`)
 
 	function addShadow() {
 		if (window.scrollY >= 100) {
@@ -10,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
     
+allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+
+
     window.addEventListener('scroll', addShadow)
 
 })
