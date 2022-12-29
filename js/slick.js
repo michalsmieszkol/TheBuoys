c4e31bde60567203d2ac925fd3dbdaa14eedb9f3
Michal <michalsmieszkol@gmail.com>
2022-12-29 12:51:46 +0100

    new sections

---
+++ js/slick.js
@@ -0,0 +1,23 @@
+$('.team-carousel').slick({
+	autoplay: true,
+	arrows: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	mobileFirst: true,
+	responsive: [
+		{
+			breakpoint: 568,
+			settings: { slidesToShow: 2 }
+		},
+		{
+			breakpoint: 992,
+			settings: { slidesToShow: 3 }
+		},
+        {
+			breakpoint: 1200,
+			settings: { slidesToShow: 5 }
+		},
+        
+	]
+});
+
